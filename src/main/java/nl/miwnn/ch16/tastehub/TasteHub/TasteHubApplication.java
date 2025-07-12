c368d9092d0beed564912944b961f76d1fb711dc
Dennis Mei <dmei22@hotmail.com>
2025-07-12 15:26:46 +0200

    Initial commit

---
+++ src/main/java/nl/miwnn/ch16/tastehub/TasteHub/TasteHubApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch16.tastehub.TasteHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasteHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/ch16/tastehub/TasteHub/TasteHubApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch16.tastehub.TasteHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
